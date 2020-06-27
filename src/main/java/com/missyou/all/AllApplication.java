aa5112678869775860d187c3fc5245c343f443da
skboy <364971779@qq.com>
2020-06-27 14:45:39 +0800

    pom

---
+++ src/main/java/com/missyou/all/AllApplication.java
@@ -0,0 +1,30 @@
+package com.missyou.all;
+
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * 千锋达摩院单体应用打包
+ * @author Lusifer
+ * @since v1.0.0
+ */
+@SpringBootApplication(scanBasePackages = { "com.missyou" })
+@MapperScan(basePackages = "com.missyou.repository.**.mapper")
+public class AllApplication {
+    /**
+     * MyBatis Plus 分页插件
+     * @return {@link PaginationInterceptor}
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllApplication.class, args);
+    }
+}
